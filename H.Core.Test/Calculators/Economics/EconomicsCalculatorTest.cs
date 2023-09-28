7a4497aa71f2ce6cf23ea6d4c1a642fa4dd4198a
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2023-09-28 12:10:54 -0600

    Change path to fix where file is stored in non-windows operating systems

--- H.Core.Test/Calculators/Economics/EconomicsCalculatorTest.cs
+++ H.Core.Test/Calculators/Economics/EconomicsCalculatorTest.cs
@@ -121,8 +121,7 @@ namespace H.Core.Test.Calculators.Economics
             #endregion
 
             var barleyMetricFile = "barleyMetricEcon.csv";
-            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var path = Path.Combine(localAppData, "Temp");
+            var path = Path.GetTempPath();
 
             var applicationData = new ApplicationData();
             applicationData.DisplayUnitStrings = new DisplayUnitStrings();
