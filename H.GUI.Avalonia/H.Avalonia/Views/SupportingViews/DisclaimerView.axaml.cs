1f8ada5cb58cd1565e415a64d0059753d312aa6c
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2023-10-05 15:28:28 -0600

    Add a disclaimer view

---
+++ H.GUI.Avalonia/H.Avalonia/Views/SupportingViews/DisclaimerView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace H.Avalonia.Views.SupportingViews;
+
+public partial class DisclaimerView : UserControl
+{
+    public DisclaimerView()
+    {
+        InitializeComponent();
+    }
+}
