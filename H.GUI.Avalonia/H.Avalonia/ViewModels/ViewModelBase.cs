7ecf8f7af4fb6c18118af3eb4852694e086f0600
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2023-10-11 16:57:47 -0600

    Add some properties and constructors from v4 ViewModelBase

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ViewModelBase.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ViewModelBase.cs
@@ -6,12 +6,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Controls.Notifications;
+using H.Core.Calculators.UnitsOfMeasurement;
+using H.Core.Enumerations;
+using Prism.Commands;
+using Prism.Events;
 
 namespace H.Avalonia.ViewModels
 {
     public class ViewModelBase : BindableBase, INavigationAware
     {
+        #region Fields
+        private string _title;
+        private string _subtitle;
         private Storage? _storage;
+
+        public IRegionManager RegionManager;
+
+        //private ObservableCollection<Farm> _copiedFarmsUsedForSimulation;
+        private EmissionDisplayUnits _selectedEmissionDisplayUnits;
+
+        #endregion
 
         protected ViewModelBase()
         {
@@ -30,7 +44,42 @@ namespace H.Avalonia.ViewModels
         {
             Storage = storage;
         }
+        
+        protected ViewModelBase(IRegionManager? regionManager, IEventAggregator? eventAggregator, Storage? storage)
+        {
+            if (regionManager != null)
+            {
+                this.RegionManager = regionManager;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(regionManager));
+            }
 
+            if (eventAggregator != null)
+            {
+                this.EventAggregator = eventAggregator;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(eventAggregator));
+            }
+
+            if (storage != null)
+            {
+                this.Storage = storage;
+                //this.Storage.ApplicationData.GlobalSettings.PropertyChanged += GlobalSettingsOnPropertyChanged;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(storage));
+            }
+
+            this.Construct();
+        }
+
+        
+        #region Properties
         /// <summary>
         /// A storage file that contains various data items that are shored between viewmodels are passed around the system. This storage
         /// item is instantiated using Prism and through Dependency Injection, is passed within the system.
@@ -41,11 +90,68 @@ namespace H.Avalonia.ViewModels
             set => SetProperty(ref _storage, value);
         }
         
+        
+        public IEventAggregator EventAggregator { get; set; }
+
+        public DelegateCommand<object> OkCommand { get; set; }
+        public DelegateCommand<object> CancelCommand { get; set; }
+        public DelegateCommand NextCommand { get; set; }
+        public DelegateCommand BackCommand { get; set; }
+        public DelegateCommand ResultsCommand { get; set; }
+        public DelegateCommand ResetStageState { get; set; }
+
+        /// <summary>
+        /// The title of the view.
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        /// <summary>
+        /// The subtitle of the view.
+        /// </summary>
+        public string Subtitle
+        {
+            get => _subtitle;
+            set => SetProperty(ref _subtitle, value);
+        }
+        
+        public bool WindowWasCancelled { get; set; }
+
+        // Active farm property must be readonly so that all views and view models get the same instance of the ActiveFarm property from the GlobalSettings instance. 
+        // If this isn't readonly, then each view model will get a different instance of this property and the instances will be out of sync when one copy of the active farm is updated.
+        // The issue is that an instance of this class 'ViewModelBase' is created for each class that inherits from it which is pretty much all view models - therefore unless this property
+        // is readonly (returns same object each time), then all view models will get different copies of the ActiveFarm which is not what we want.
+        // public Farm ActiveFarm
+        // {
+        //     get { return this.Storage.ApplicationData.GlobalSettings.ActiveFarm; }
+        // }
+
+        // public ObservableCollection<Farm> CopiedFarmsUsedForSimulation
+        // {
+        //     get { return _copiedFarmsUsedForSimulation; }
+        //     set { SetProperty(ref _copiedFarmsUsedForSimulation, value); }
+        // }
+
+        public EmissionDisplayUnits SelectedEmissionDisplayUnits
+        {
+            get => _selectedEmissionDisplayUnits;
+            set => SetProperty(ref _selectedEmissionDisplayUnits, value);
+        }
+
+        public UnitsOfMeasurementCalculator UnitCalculator { get; set; }
+        
+        
         /// <summary>
         /// The notification manager that handles displaying notifications on the page.
         /// </summary>
         public WindowNotificationManager NotificationManager { get; set; }
         
+        #endregion
+        
+        #region Public Methods
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
             return true;
@@ -67,5 +173,17 @@ namespace H.Avalonia.ViewModels
         {
             return true;
         }
+        
+        public void Construct()
+        {
+
+            //this.CopiedFarmsUsedForSimulation = new ObservableCollection<Farm>();
+
+            //this.ResultsCommand = new DelegateCommand(this.OnResults);
+
+            this.UnitCalculator = new UnitsOfMeasurementCalculator();
+        }
+        
+        #endregion
     }
 }
