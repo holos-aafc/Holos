c2fc9e87a23b999ad8d7a759d3b5b035dbf5864e
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2023-10-12 16:05:40 -0600

    Return value in EnumDescriptionConverter.cs when converting description back

--- H.Infrastructure/Controls/ValueConverters/EnumDescriptionConverter.cs
+++ H.Infrastructure/Controls/ValueConverters/EnumDescriptionConverter.cs
@@ -66,7 +66,7 @@ namespace H.Infrastructure.Controls.ValueConverters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return value;
         }
 
         #endregion
