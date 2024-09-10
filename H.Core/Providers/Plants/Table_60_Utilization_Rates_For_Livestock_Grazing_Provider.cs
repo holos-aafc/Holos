82a987d27312f7ea2f05debb6b13b1570a32761c
sarahp1980 <112989918+sarahp1980@users.noreply.github.com>
2024-09-10 10:29:50 -0600

    Update Table_60_Utilization_Rates_For_Livestock_Grazing_Provider.cs
    Added source for data in this table

--- H.Core/Providers/Plants/Table_60_Utilization_Rates_For_Livestock_Grazing_Provider.cs
+++ H.Core/Providers/Plants/Table_60_Utilization_Rates_For_Livestock_Grazing_Provider.cs
@@ -14,6 +14,7 @@ namespace H.Core.Providers.Plants
     /// 
     /// A revised utilization rate table for grazed systems, where the utilization rate depends on the type of grazed perennial
     /// forage (rather than the grazing regime or stocking density), and this in turn can be used to back-calculate above ground pasture biomass.
+    /// <para>Source: Bailey et al. (2010) (for native rangeland utilization rate); Expert opinion</para>
     /// </summary>
     public class Table_60_Utilization_Rates_For_Livestock_Grazing_Provider
     {
