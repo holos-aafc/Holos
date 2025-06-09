c2709783ffda5d0487b27c1f432176e286bf11b9
Aaron McPherson <95769272+holos-aafc@users.noreply.github.com>
2025-06-09 14:02:27 -0600

    Calculate Cpreg during all periods
    #344

--- H.Core/Services/Animals/SheepResultsService.cs
+++ H.Core/Services/Animals/SheepResultsService.cs
@@ -130,7 +130,7 @@ namespace H.Core.Services.Animals
                 }
             }
 
-            if (animalGroup.GroupType.IsPregnantType() && managementPeriod.ProductionStage == ProductionStages.Gestating)
+            if (animalGroup.GroupType.IsPregnantType() && (managementPeriod.ProductionStage == ProductionStages.Gestating || managementPeriod.ProductionStage == ProductionStages.Lactating))
             {
                 var pregnancyCoefficient = _pregnancyCoefficientProvider.GetPregnancyCoefficient(
                     lambRatio: dailyEmissions.LambEweRatio);
