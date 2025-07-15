53e9d1c468b8a9e7385ee5cbd496ee5f498cb257
Aaron McPherson <95769272+holos-aafc@users.noreply.github.com>
2025-07-15 11:45:34 -0600

    Move CLI N input calculations

--- H.Core/Calculators/Nitrogen/NitrogenService/NitrogenService.cs
+++ H.Core/Calculators/Nitrogen/NitrogenService/NitrogenService.cs
@@ -175,59 +175,6 @@ namespace H.Core.Calculators.Nitrogen.NitrogenService
             return viewItems.Sum(x => x.BelowgroundNitrogenInputs);
         }
 
-        public void ProcessCommandLineItems(List<CropViewItem> viewItems, Farm farm)
-        {
-            if (farm.IsCommandLineMode == false)
-            {
-                return;
-            }
-
-            for (int i = 0; i < viewItems.Count; i++)
-            {
-                var itemAtIndex = viewItems.ElementAt(i);
-                var year = itemAtIndex.Year;
-                var tuple = _fieldComponentHelper.GetAdjoiningYears(viewItems, year);
-
-                var currentYearViewItem = tuple.CurrentYearViewItem;
-                var previousYearViewItem = tuple.PreviousYearViewItem;
-
-                if (currentYearViewItem.CombinedAboveGroundResidueNitrogen == 0 && currentYearViewItem.CombinedBelowGroundResidueNitrogen == 0)
-                {
-                    // User has not provided these values, we need to assign the necessary prerequisite values before calculating N inputs
-                    if (currentYearViewItem.NitrogenContentInProduct == 0)
-                    {
-                        _cropInitializationService.InitializeNitrogenContentInProduct(currentYearViewItem, farm);
-                    }
-
-                    if (currentYearViewItem.NitrogenContentInStraw == 0)
-                    {
-                        _cropInitializationService.InitializeNitrogenContentInStraw(currentYearViewItem, farm);
-                    }
-
-                    if (currentYearViewItem.NitrogenContentInRoots == 0)
-                    {
-                        _cropInitializationService.InitializeNitrogenContentInRoots(currentYearViewItem, farm);
-                    }
-
-                    if (currentYearViewItem.NitrogenContentInExtraroot == 0)
-                    {
-                        _cropInitializationService.InitializeNitrogenContentInExtraroots(currentYearViewItem, farm);
-                    }
-
-                    if (currentYearViewItem.NitrogenContent == 0)
-                    {
-                        _cropInitializationService.InitializeIPCCNitrogenContent(currentYearViewItem, farm);
-                    }
-
-                    _cropInitializationService.InitializeNitrogenFixation(currentYearViewItem);
-
-                    this.AssignNitrogenInputs(currentYearViewItem, farm, previousYearViewItem);
-                }
-            }
-        }
-
-
-
         #endregion
 
         #region Private Methods
